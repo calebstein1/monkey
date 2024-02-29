ac82d62d0bd6b46c35eb5016e1984e18d133c7b5
Caleb Stein <caleb@calebstein.net>
2024-02-29 15:06:44 -0800

    lexer reads in identifiers

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -32,9 +32,28 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 	l.ReadChar()
 	return tok
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.ReadChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_' || ch == '?'
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
