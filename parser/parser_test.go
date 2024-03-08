cf101434bd5e9acf2a4dd71ddb11904c94779bd9
Caleb Stein <caleb@calebstein.net>
2024-03-07 17:37:31 -0800

    add initial test for parsing return statements

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -6,6 +6,34 @@ import (
 	"testing"
 )
 
+func TestReturnStatements(t *testing.T) {
+	input := `
+return 5;
+return 10;
+return 993322;
+`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("Program does not contain 3 statements. Got %d", len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.ReturnStatement. got %T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteran not 'return'. got %q", returnStmt.TokenLiteral())
+		}
+	}
+}
+
 func TestLetStatements(t *testing.T) {
 	input := `
 let x = 5;
